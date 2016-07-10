afe7f566af869e6a7a4688c0eab31d329dc6fa80
Lukáš Černý <lukas.cerny@futuretek.cz>
2016-07-10 20:18:59 +0200

    Fix

--- FontAwesomePicker.php
+++ FontAwesomePicker.php
@@ -3,6 +3,7 @@
 namespace futuretek\widgets;
 
 use yii\bootstrap\InputWidget;
+use yii\helpers\Html;
 use yii\web\View;
 
 /**
@@ -15,23 +16,38 @@ use yii\web\View;
  */
 class FontAwesomePicker extends InputWidget
 {
+    const PLACEMENT_INLINE = 'inline';
+    const PLACEMENT_TOP_LEFT_CORNER = 'topLeftCorner';
+    const PLACEMENT_TOP_LEFT = 'topLeft';
+    const PLACEMENT_TOP = 'top';
+    const PLACEMENT_TOP_RIGHT = 'topRight';
+    const PLACEMENT_TOP_RIGHT_CORNER = 'topRightCorner';
+    const PLACEMENT_RIGHT_TOP = 'rightTop';
+    const PLACEMENT_RIGHT = 'right';
+    const PLACEMENT_RIGHT_BOTTOM = 'rightBottom';
+    const PLACEMENT_BOTTOM_RIGHT_CORNER = 'bottomRightCorner';
+    const PLACEMENT_BOTTOM_RIGHT = 'bottomRight';
+    const PLACEMENT_BOTTOM = 'bottom';
+    const PLACEMENT_BOTTOM_LEFT = 'bottomLeft';
+    const PLACEMENT_BOTTOM_LEFT_CORNER = 'bottomLeftCorner';
+    const PLACEMENT_LEFT_BOTTOM = 'leftBottom';
+    const PLACEMENT_LEFT = 'left';
+    const PLACEMENT_LEFT_TOP = 'leftTop';
+
+    public $placement = self::PLACEMENT_BOTTOM_RIGHT;
+
     /**
      * @inheritdoc
      */
     public function run()
     {
-        $this->registerAssets();
-        parent::run();
-    }
+        FontAwesomePickerAsset::register($this->view);
+        $this->view->registerCss('div.iconpicker-items > div.tooltip { display:none!important; }', [], 'iconpicker-tooltip');
+        $this->view->registerJs('$("#' . $this->options['id'] . '").iconpicker({
+        placement: "' . $this->placement . '"
+        });', View::POS_READY);
 
-    /**
-     * Register assets
-     */
-    protected function registerAssets()
-    {
-        $view = $this->getView();
-        FontAwesomePickerAsset::register($view);
-
-        $view->registerJs('$("#' . $this->id . '").iconpicker();', View::POS_READY);
+        Html::addCssClass($this->options, 'form-control');
+        echo $this->hasModel() ? Html::activeTextInput($this->model, $this->attribute, $this->options) : Html::textInput($this->name, $this->value, $this->options);
     }
 }
